692e7ed6d1816a592b4dda9d04eef7d79a1d9a28
NorthernSantan <80981213+NorthernSantan@users.noreply.github.com>
2021-03-31 14:33:26 -0400

    Update contact-form-handler.php

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,20 +1,20 @@
 <?php
   $fname = $_POST['fname'];  
   $lname = $_POST['lname'];
-  $email = $_POST['email'];
+  $contact_email = $_POST['contact_email'];
   $country = $_POST['country'];
   $message = $_POST['subject'];
 
   $email_from = 'colin@slpv2.org';
   $email_subject = "New form submission";
   $email_body = "User Name: $name.\n".
-    "User Email: $email.\n"
+    "User Email: $contact_email.\n"
       "User Message: $message.\n";
 
 $to = "colin.william.mccormack@gmail.com;
 
 $headers = "From: $email_from \r\n";
-$headers .= "Reply-To: $email \r\n";
+$headers .= "Reply-To: $contact_email \r\n";
 mail($to,$email_subject,$email_body,$headers);
 
 header("Location: contact.html");
