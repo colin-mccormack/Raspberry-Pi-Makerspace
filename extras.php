24129fa5dcbf5a4178c58c12e4b39109e3619e89
NorthernSantan <80981213+NorthernSantan@users.noreply.github.com>
2021-05-02 14:04:18 -0400

    Update extras.php

--- extras.php
+++ extras.php
@@ -91,7 +91,7 @@
       		  print("<th>Name</th>"); 
 	          print("</tr><tr>");
 	          print("</tr>");
-		  print("<td>" . $row->name[0] . "</td>");
+		  print("<td>" . $row[0]->name . "</td>");
 		  print("</table>");
 
       /*print("<br><br><br>");
