ccd81993b74831b5ace7ed9361a3602516cf5c5f
NorthernSantan <80981213+NorthernSantan@users.noreply.github.com>
2021-05-04 09:48:23 -0400

    Update extras.php

--- extras.php
+++ extras.php
@@ -40,259 +40,261 @@
       		<input type="submit" name="submit1">
       		<label>Search By Atomic Weight</label>
       		<input type="text" name="search2">
-      		<input type="submit" name="submit2">
+      		<input type="submit" name="submit">
       	</form>
 	<!-- <img src="version-periodic-table-elements.jpg" alt="Periodic Table from Encyclopedia Britannica" style="height: 250px; float: right;"/> -->
 <?php
+	if (isset($_POST["submit1"]) || isset($_POST["submit2"])) {
 
-  $con = new PDO("mysql:host=localhost;dbname=Chemistry;charset=utf8",'colin','lego');
-	if (isset($_POST["submit1"]) && isset($_POST["submit2"]))
-	{
-	    $str = $_POST["search1"];
-	    $sth = $con->prepare("SELECT * FROM Elements WHERE name = '$str' OR symbol ='$str' OR CAST(atmwight as CHAR) LIKE '$str%'");
+		$con = new PDO("mysql:host=localhost;dbname=Chemistry;charset=utf8",'colin','lego');
+		if (isset($_POST["search1"]) && isset($_POST["search2"]))
+		{
+		    $str = $_POST["search1"];
+		    $sth = $con->prepare("SELECT * FROM Elements WHERE name = '$str' OR symbol ='$str' OR CAST(atmwight as CHAR) LIKE '$str%'");
 
-  	    $sth->setFetchMode(PDO:: FETCH_ASSOC);
-   	    $sth -> execute();
-
-	    if($row = $sth->fetch())
-	    {
-
-	      print("<br><br><br>");
-	      print("<table>");
-	      print("<tr>");
-	      print("<th>Name</th>");
-	      print("<th>Symbol</th>");
-	      print("<th>Atomic Number</th>");
-	      print("<th>Atomic Weight</th>");
-	      print("<th>Melting Point (Degrees Celsius)</th>");
-	      print("<th>Boiling Point (Degrees Celsius)</th>");	      
-	      print("<th>Density (g/cm3)</th>");
-	      print("<th>Group Number</th>");
-	      print("<th>Configuration</th>");
-	      print("<th>Ionization Energy (eV)</th>");
-	      print("<th>Charges</th>");
-	      print("<th>Multivalent</th>");
-	      print("<th>Phase (State)</th>");
-	      print("<th>Atomic Radius</th>");
-	      print("<th>Covalent Radius</th>");
-	      print("<th>Electron Affinity</th>");
-	      print("<th>Electronegativity</th>");
-	      print("<th>Molar Volume</th>");    
-	      print("</tr><tr>");
-	      print("<td>" . $row['name'] . "</td>");
-	      print("<td>" . $row['symbol'] ."</td>");
-	      print("<td>" . $row['atmnum'] ."</td>");
-	      print("<td>" . $row['atmweight'] . "</td>");
-	      print("<td>" . $row['melting'] . "</td>");
-	      print("<td>" . $row['boiling'] . "</td>");
-	      print("<td>" . $row['density'] . "</td>");
-	      print("<td>" . $row['groupnum'] ."</td>");
-	      print("<td>" . $row['configuration'] ."</td>");
-	      print("<td>" . $row['ie'] . "</td>");
-	      print("<td>" . $row['charge'] . "</td>");
-	      print("<td>" . $row['valences'] . "</td>");
-	      print("<td>" . $row['phase'] . "</td>");
-	      print("<td>" . $row['ar'] ."</td>");
-	      print("<td>" . $row['cr'] ."</td>");
-	      print("<td>" . $row['ea'] . "</td>");
-	      print("<td>" . $row['en'] . "</td>");
-	      print("<td>" . $row['mv'] . "</td>");
-	      print("</tr>");
-	      print("</table>");
-		    
-		    $sth = $con->prepare("SELECT * FROM Abundance WHERE name = '$str' OR symbol = '$str'");
-		    $sth->fetch(PDO::FETCH_ASSOC);
+		    $sth->setFetchMode(PDO:: FETCH_ASSOC);
 		    $sth -> execute();
-			 if($row = $sth)
-			    {
-			      print("<br><br><br>");
-			      print("<table>");
-			      print("<tr>");
-			      print("<th>Name</th>");
-			      print("<th>Symbol</th>");
-			      print("<th>Average Mass</th>");
-			      print("<th>Mass</th>");
-			      print("<th>Abundance %</th>");
-			      print("</tr>");
-				  while($row = $sth->fetch())
+
+		    if($row = $sth->fetch())
+		    {
+
+		      print("<br><br><br>");
+		      print("<table>");
+		      print("<tr>");
+		      print("<th>Name</th>");
+		      print("<th>Symbol</th>");
+		      print("<th>Atomic Number</th>");
+		      print("<th>Atomic Weight</th>");
+		      print("<th>Melting Point (Degrees Celsius)</th>");
+		      print("<th>Boiling Point (Degrees Celsius)</th>");	      
+		      print("<th>Density (g/cm3)</th>");
+		      print("<th>Group Number</th>");
+		      print("<th>Configuration</th>");
+		      print("<th>Ionization Energy (eV)</th>");
+		      print("<th>Charges</th>");
+		      print("<th>Multivalent</th>");
+		      print("<th>Phase (State)</th>");
+		      print("<th>Atomic Radius</th>");
+		      print("<th>Covalent Radius</th>");
+		      print("<th>Electron Affinity</th>");
+		      print("<th>Electronegativity</th>");
+		      print("<th>Molar Volume</th>");    
+		      print("</tr><tr>");
+		      print("<td>" . $row['name'] . "</td>");
+		      print("<td>" . $row['symbol'] ."</td>");
+		      print("<td>" . $row['atmnum'] ."</td>");
+		      print("<td>" . $row['atmweight'] . "</td>");
+		      print("<td>" . $row['melting'] . "</td>");
+		      print("<td>" . $row['boiling'] . "</td>");
+		      print("<td>" . $row['density'] . "</td>");
+		      print("<td>" . $row['groupnum'] ."</td>");
+		      print("<td>" . $row['configuration'] ."</td>");
+		      print("<td>" . $row['ie'] . "</td>");
+		      print("<td>" . $row['charge'] . "</td>");
+		      print("<td>" . $row['valences'] . "</td>");
+		      print("<td>" . $row['phase'] . "</td>");
+		      print("<td>" . $row['ar'] ."</td>");
+		      print("<td>" . $row['cr'] ."</td>");
+		      print("<td>" . $row['ea'] . "</td>");
+		      print("<td>" . $row['en'] . "</td>");
+		      print("<td>" . $row['mv'] . "</td>");
+		      print("</tr>");
+		      print("</table>");
+
+			    $sth = $con->prepare("SELECT * FROM Abundance WHERE name = '$str' OR symbol = '$str'");
+			    $sth->fetch(PDO::FETCH_ASSOC);
+			    $sth -> execute();
+				 if($row = $sth)
 				    {
+				      print("<br><br><br>");
+				      print("<table>");
 				      print("<tr>");
-				      print("<td>" . $row['name'] . "</td>");
-				      print("<td>" . $row['symbol'] . "</td>");
-				      print("<td>" . $row['avgweight'] . "</td>");
-				      print("<td>" . $row['mass'] . "</td>");
-				      print("<td>" . $row['abundance'] . "</td>");
+				      print("<th>Name</th>");
+				      print("<th>Symbol</th>");
+				      print("<th>Average Mass</th>");
+				      print("<th>Mass</th>");
+				      print("<th>Abundance %</th>");
 				      print("</tr>");
-				    }
-			      print("</table>");
-			 }
-	    }
-	    else
-	    {
-		    print("<br><br><br>");
-		    echo "Name Does Not Match Abundance Table";
-	    }  
-	
-	}
-	elseif (isset($_POST["submit1"]))
-	{
-	     $str = $_POST["search1"];
-	     $sth = $con->prepare("SELECT * FROM Elements WHERE name = '$str' OR symbol ='$str'");
+					  while($row = $sth->fetch())
+					    {
+					      print("<tr>");
+					      print("<td>" . $row['name'] . "</td>");
+					      print("<td>" . $row['symbol'] . "</td>");
+					      print("<td>" . $row['avgweight'] . "</td>");
+					      print("<td>" . $row['mass'] . "</td>");
+					      print("<td>" . $row['abundance'] . "</td>");
+					      print("</tr>");
+					    }
+				      print("</table>");
+				 }
+		    }
+		    else
+		    {
+			    print("<br><br><br>");
+			    echo "Name Does Not Match Abundance Table";
+		    }  
 
- 	    $sth->setFetchMode(PDO:: FETCH_OBJ);
-	    $sth -> execute();
+		}
+		elseif (isset($_POST["search1"]))
+		{
+		     $str = $_POST["search1"];
+		     $sth = $con->prepare("SELECT * FROM Elements WHERE name = '$str' OR symbol ='$str'");
 
-	
-	    if($row = $sth->fetch())
-	    {
-
-	      print("<br><br><br>");
-	      print("<table>");
-	      print("<tr>");
-	      print("<th>Name</th>");
-	      print("<th>Symbol</th>");
-	      print("<th>Atomic Number</th>");
-	      print("<th>Atomic Weight</th>");
-	      print("<th>Melting Point (Degrees Celsius)</th>");
-	      print("<th>Boiling Point (Degrees Celsius)</th>");	      
-	      print("<th>Density (g/cm3)</th>");
-	      print("<th>Group Number</th>");
-	      print("<th>Configuration</th>");
-	      print("<th>Ionization Energy (eV)</th>");
-	      print("<th>Charges</th>");
-	      print("<th>Multivalent</th>");
-	      print("<th>Phase (State)</th>");
-	      print("<th>Atomic Radius</th>");
-	      print("<th>Covalent Radius</th>");
-	      print("<th>Electron Affinity</th>");
-	      print("<th>Electronegativity</th>");
-	      print("<th>Molar Volume</th>");    
-	      print("</tr><tr>");
-	      print("<td>" . $row->name . "</td>");
-	      print("<td>" . $row->symbol ."</td>");
-	      print("<td>" . $row->atmnum ."</td>");
-	      print("<td>" . $row->atmweight . "</td>");
-	      print("<td>" . $row->melting . "</td>");
-	      print("<td>" . $row->boiling . "</td>");
-	      print("<td>" . $row->density . "</td>");
-	      print("<td>" . $row->groupnum ."</td>");
-	      print("<td>" . $row->configuration ."</td>");
-	      print("<td>" . $row->ie . "</td>");
-	      print("<td>" . $row->charge . "</td>");
-	      print("<td>" . $row->valences . "</td>");
-	      print("<td>" . $row->phase . "</td>");
-	      print("<td>" . $row->ar ."</td>");
-	      print("<td>" . $row->cr ."</td>");
-	      print("<td>" . $row->ea . "</td>");
-	      print("<td>" . $row->en . "</td>");
-	      print("<td>" . $row->mv . "</td>");
-	      print("</tr>");
-	      print("</table>");
-		    
-		    $sth = $con->prepare("SELECT * FROM Abundance WHERE name = '$str' OR symbol = '$str'");
-		    $sth->fetch(PDO::FETCH_ASSOC);
+		    $sth->setFetchMode(PDO:: FETCH_OBJ);
 		    $sth -> execute();
-			 if($row = $sth)
-			    {
-			      print("<br><br><br>");
-			      print("<table>");
-			      print("<tr>");
-			      print("<th>Name</th>");
-			      print("<th>Symbol</th>");
-			      print("<th>Average Mass</th>");
-			      print("<th>Mass</th>");
-			      print("<th>Abundance %</th>");
-			      print("</tr>");
-				  while($row = $sth->fetch())
+
+
+		    if($row = $sth->fetch())
+		    {
+
+		      print("<br><br><br>");
+		      print("<table>");
+		      print("<tr>");
+		      print("<th>Name</th>");
+		      print("<th>Symbol</th>");
+		      print("<th>Atomic Number</th>");
+		      print("<th>Atomic Weight</th>");
+		      print("<th>Melting Point (Degrees Celsius)</th>");
+		      print("<th>Boiling Point (Degrees Celsius)</th>");	      
+		      print("<th>Density (g/cm3)</th>");
+		      print("<th>Group Number</th>");
+		      print("<th>Configuration</th>");
+		      print("<th>Ionization Energy (eV)</th>");
+		      print("<th>Charges</th>");
+		      print("<th>Multivalent</th>");
+		      print("<th>Phase (State)</th>");
+		      print("<th>Atomic Radius</th>");
+		      print("<th>Covalent Radius</th>");
+		      print("<th>Electron Affinity</th>");
+		      print("<th>Electronegativity</th>");
+		      print("<th>Molar Volume</th>");    
+		      print("</tr><tr>");
+		      print("<td>" . $row->name . "</td>");
+		      print("<td>" . $row->symbol ."</td>");
+		      print("<td>" . $row->atmnum ."</td>");
+		      print("<td>" . $row->atmweight . "</td>");
+		      print("<td>" . $row->melting . "</td>");
+		      print("<td>" . $row->boiling . "</td>");
+		      print("<td>" . $row->density . "</td>");
+		      print("<td>" . $row->groupnum ."</td>");
+		      print("<td>" . $row->configuration ."</td>");
+		      print("<td>" . $row->ie . "</td>");
+		      print("<td>" . $row->charge . "</td>");
+		      print("<td>" . $row->valences . "</td>");
+		      print("<td>" . $row->phase . "</td>");
+		      print("<td>" . $row->ar ."</td>");
+		      print("<td>" . $row->cr ."</td>");
+		      print("<td>" . $row->ea . "</td>");
+		      print("<td>" . $row->en . "</td>");
+		      print("<td>" . $row->mv . "</td>");
+		      print("</tr>");
+		      print("</table>");
+
+			    $sth = $con->prepare("SELECT * FROM Abundance WHERE name = '$str' OR symbol = '$str'");
+			    $sth->fetch(PDO::FETCH_ASSOC);
+			    $sth -> execute();
+				 if($row = $sth)
 				    {
+				      print("<br><br><br>");
+				      print("<table>");
 				      print("<tr>");
-				      print("<td>" . $row['name'] . "</td>");
-				      print("<td>" . $row['symbol'] . "</td>");
-				      print("<td>" . $row['avgweight'] . "</td>");
-				      print("<td>" . $row['mass'] . "</td>");
-				      print("<td>" . $row['abundance'] . "</td>");
+				      print("<th>Name</th>");
+				      print("<th>Symbol</th>");
+				      print("<th>Average Mass</th>");
+				      print("<th>Mass</th>");
+				      print("<th>Abundance %</th>");
 				      print("</tr>");
-				    }
-			      print("</table>");
-			 }
-	    }
-	    else
-	    {
-		    print("<br><br><br>");
-		    echo "Name Does Not Match Submit1 Table";
-	    }  
-	    
-	 
-	}   
-	elseif (isset($_POST["submit2"]))
-	{
-		$str = $_POST["search2"];
-		$sth = $con->prepare("SELECT * FROM Elements WHERE CAST(atmwight as CHAR) LIKE '$str%'");
-
-   		 $sth->setFetchMode(PDO:: FETCH_OBJ);
-   		 $sth -> execute();
-	    print("\nIn Submit2\n");
+					  while($row = $sth->fetch())
+					    {
+					      print("<tr>");
+					      print("<td>" . $row['name'] . "</td>");
+					      print("<td>" . $row['symbol'] . "</td>");
+					      print("<td>" . $row['avgweight'] . "</td>");
+					      print("<td>" . $row['mass'] . "</td>");
+					      print("<td>" . $row['abundance'] . "</td>");
+					      print("</tr>");
+					    }
+				      print("</table>");
+				 }
+		    }
+		    else
+		    {
+			    print("<br><br><br>");
+			    echo "Name Does Not Match Submit1 Table";
+		    }  
 
 
-	    if($row = $sth->fetch())
-	    {
-		    	    print("\nIn Submit2 Searching\n");
+		}   
+		elseif (isset($_POST["search2"]))
+		{
+			$str = $_POST["search2"];
+			$sth = $con->prepare("SELECT * FROM Elements WHERE CAST(atmwight as CHAR) LIKE '$str%'");
+
+			 $sth->setFetchMode(PDO:: FETCH_OBJ);
+			 $sth -> execute();
+		    print("\nIn Submit2\n");
 
 
-	      print("<br><br><br>");
-	      print("<table>");
-	      print("<tr>");
-	      print("<th>Name</th>");
-	      print("<th>Symbol</th>");
-	      print("<th>Atomic Number</th>");
-	      print("<th>Atomic Weight</th>");
-	      print("<th>Melting Point (Degrees Celsius)</th>");
-	      print("<th>Boiling Point (Degrees Celsius)</th>");	      
-	      print("<th>Density (g/cm3)</th>");
-	      print("<th>Group Number</th>");
-	      print("<th>Configuration</th>");
-	      print("<th>Ionization Energy (eV)</th>");
-	      print("<th>Charges</th>");
-	      print("<th>Multivalent</th>");
-	      print("<th>Phase (State)</th>");
-	      print("<th>Atomic Radius</th>");
-	      print("<th>Covalent Radius</th>");
-	      print("<th>Electron Affinity</th>");
-	      print("<th>Electronegativity</th>");
-	      print("<th>Molar Volume</th>");    
-	      print("</tr><tr>");
-	      print("<td>" . $row->name . "</td>");
-	      print("<td>" . $row->symbol ."</td>");
-	      print("<td>" . $row->atmnum ."</td>");
-	      print("<td>" . $row->atmweight . "</td>");
-	      print("<td>" . $row->melting . "</td>");
-	      print("<td>" . $row->boiling . "</td>");
-	      print("<td>" . $row->density . "</td>");
-	      print("<td>" . $row->groupnum ."</td>");
-	      print("<td>" . $row->configuration ."</td>");
-	      print("<td>" . $row->ie . "</td>");
-	      print("<td>" . $row->charge . "</td>");
-	      print("<td>" . $row->valences . "</td>");
-	      print("<td>" . $row->phase . "</td>");
-	      print("<td>" . $row->ar ."</td>");
-	      print("<td>" . $row->cr ."</td>");
-	      print("<td>" . $row->ea . "</td>");
-	      print("<td>" . $row->en . "</td>");
-	      print("<td>" . $row->mv . "</td>");
-	      print("</tr>");
-	      print("</table>");
-	    }
-	    else
-	    {
-		    print("<br><br><br>");
-		    echo "Name Does Not Match Submit2 Table";
-	    }
+		    if($row = $sth->fetch())
+		    {
+				    print("\nIn Submit2 Searching\n");
+
+
+		      print("<br><br><br>");
+		      print("<table>");
+		      print("<tr>");
+		      print("<th>Name</th>");
+		      print("<th>Symbol</th>");
+		      print("<th>Atomic Number</th>");
+		      print("<th>Atomic Weight</th>");
+		      print("<th>Melting Point (Degrees Celsius)</th>");
+		      print("<th>Boiling Point (Degrees Celsius)</th>");	      
+		      print("<th>Density (g/cm3)</th>");
+		      print("<th>Group Number</th>");
+		      print("<th>Configuration</th>");
+		      print("<th>Ionization Energy (eV)</th>");
+		      print("<th>Charges</th>");
+		      print("<th>Multivalent</th>");
+		      print("<th>Phase (State)</th>");
+		      print("<th>Atomic Radius</th>");
+		      print("<th>Covalent Radius</th>");
+		      print("<th>Electron Affinity</th>");
+		      print("<th>Electronegativity</th>");
+		      print("<th>Molar Volume</th>");    
+		      print("</tr><tr>");
+		      print("<td>" . $row->name . "</td>");
+		      print("<td>" . $row->symbol ."</td>");
+		      print("<td>" . $row->atmnum ."</td>");
+		      print("<td>" . $row->atmweight . "</td>");
+		      print("<td>" . $row->melting . "</td>");
+		      print("<td>" . $row->boiling . "</td>");
+		      print("<td>" . $row->density . "</td>");
+		      print("<td>" . $row->groupnum ."</td>");
+		      print("<td>" . $row->configuration ."</td>");
+		      print("<td>" . $row->ie . "</td>");
+		      print("<td>" . $row->charge . "</td>");
+		      print("<td>" . $row->valences . "</td>");
+		      print("<td>" . $row->phase . "</td>");
+		      print("<td>" . $row->ar ."</td>");
+		      print("<td>" . $row->cr ."</td>");
+		      print("<td>" . $row->ea . "</td>");
+		      print("<td>" . $row->en . "</td>");
+		      print("<td>" . $row->mv . "</td>");
+		      print("</tr>");
+		      print("</table>");
+		    }
+		    else
+		    {
+			    print("<br><br><br>");
+			    echo "Name Does Not Match Submit2 Table";
+		    }
+		}
+	      else
+		{
+			echo "You need to fill in a feild";
+		}
 	}
-      else
-	{
-    		echo "You need to fill in a feild";
-	}
-    
+
    
 ?>
   </body>
