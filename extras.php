6d43edb29eab48d2581602b23479509e6105c9b9
NorthernSantan <80981213+NorthernSantan@users.noreply.github.com>
2021-05-11 19:44:32 -0400

    Update extras.php

--- extras.php
+++ extras.php
@@ -48,6 +48,7 @@
 				    <td WIDTH="75%" align="LEFT" valign="TOP"><input type="text" name="search1"></td>
 			       </tr>
 				<tr valign="TOP">
+				    <td WIDTH="25%" align="RIGHT" valign="MIDDLE"> <label></label> </td>
 				    <td WIDTH="75%" align="LEFT" valign="TOP"><input type="text" name="search2"> </td>
 			       </tr>
 			</TD></TR>
